d03797efd25390e6a233df40035cfdf58687266a
advaymansingka <38584392+advaymansingka@users.noreply.github.com>
2023-01-09 01:05:57 +0530

    full snake

--- IntroToJava/src/Snake.java
+++ IntroToJava/src/Snake.java
@@ -1,7 +1,10 @@
+// to run:
+// javac @sources.txt && java Snake
+
 public class Snake {
     public static void main(String[] args) throws Exception {
         
         new GameFrame();
-        
+
     }
 }
